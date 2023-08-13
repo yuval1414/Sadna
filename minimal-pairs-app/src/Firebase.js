d5e03ef2d84c6ae110157be633498ce87371a10d
mayab <mayab@ctera.com>
2023-08-13 15:37:10 +0300

    bfore checkin

--- minimal-pairs-app/src/Firebase.js
+++ minimal-pairs-app/src/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
+import {getStorage, getDownloadURL, ref as sref} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,81 +16,138 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const databaseRef = ref(db, 'minimal_pairs');
+// const databaseRef = ref(db, 'minimal_pairs');
 
-function writeWordData (wordId, name , db) {
-    const reference = ref(db, 'words/' + wordId);
+// function writeWordData (wordId, name , db) {
+//     const reference = ref(db, 'words/' + wordId);
     
-    set (reference, {
-        word: name        
-    });        
-}
+//     set (reference, {
+//         word: name        
+//     });        
+// }
 
-writeWordData('Monkey', 'url');
+//writeWordData('Monkey', 'url');
 
-
-// Function to add a new minimal pair to the Firebase Realtime Database
-async function addMinimalPair(databaseRef, pairData) {
-  try {    
-    const newPairRef = databaseRef.push();
-
-    await newPairRef.set(pairData);
-    console.log('New minimal pair added to the database.');
-
-    // TODO return the unique key. Not sure if we need it.
-    //return newPairRef.key;
-  } catch (error) {
-    console.error('Error adding minimal pair:', error);
-    return null;
-  }
-}
-
-async function getAllMinimalPairs(databaseRef) { 
-  try {
-    const snapshot = await databaseRef.once('value');
-    const minimalPairsData = snapshot.val();
-
-    if (minimalPairsData) {
-      return Object.values(minimalPairsData);
-    } else {
-      throw new Error('No minimal pairs found.');
-    }
-  } catch (error) {
-    console.error('Error fetching minimal pairs:', error);
-    return [];
-  }
-}
-
-const minimalPairs = getAllMinimalPairs();
-
-// Function to filter minimal pairs by sound type
-function filterPairsBySoundType(minimalPairs, soundType) {
-  return minimalPairs.filter(pair => pair.sound_type === soundType);
-}
-
-// Function to filter minimal pairs by position in word
-function filterPairsByPosition(minimalPairs, position) {
-  return minimalPairs.filter(pair => pair.position_in_word === position);
-}
-
-// Function to filter minimal pairs by a specific pair of sounds
-function filterPairsBySoundPair(minimalPairs, soundPair) {
-  return minimalPairs.filter(pair =>
-    pair.sound_pair === soundPair
-  );
-}
-
-// // Call the functions and log the filtered minimal pairs
-//
-// (async () => {
-//   const allPairs = await getAllMinimalPairs();
-//   const soundFilteredPairs = filterPairsBySoundType(allPairs, desiredSoundType);
-//   const positionFilteredPairs = filterPairsByPosition(soundFilteredPairs, desiredPosition);
-//   const soundPairFilteredPairs = filterPairsBySoundPair(positionFilteredPairs, desiredSoundPair);
-
-//   if (soundPairFilteredPairs.length > 0) {
-//     console.log(`Retrieved ${soundPairFilteredPairs.length} minimal pairs with sound type ${desiredSoundType}, position ${desiredPosition}, and sound pair ${desiredSoundPair}:`, soundPairFilteredPairs);
-//   } else {
-//     console.log(`No minimal pairs found with sound type ${desiredSoundType}, position ${desiredPosition}, and sound pair ${desiredSoundPair}.`);
+// // Function to add a new minimal pair to the Firebase Realtime Database
+// async function addMinimalPair(pairData) {
+//     try {
+//       const newPairRef = databaseRef.push();
+  
+//       // Assign a unique ID to the pair
+//       pairData.id = newPairRef.key;
+  
+//       await newPairRef.set(pairData);
+//       console.log('New minimal pair added to the database.');
+  
+//       return newPairRef.key;
+//     } catch (error) {
+//       console.error('Error adding minimal pair:', error);
+//       return null;
+//     }
 //   }
-// })();
+
+// async function getAllMinimalPairs() { 
+//   try {
+//     const snapshot = await databaseRef.once('value');
+//     const minimalPairsData = snapshot.val();
+
+//     if (minimalPairsData) {
+//       return Object.values(minimalPairsData);
+//     } else {
+//       throw new Error('No minimal pairs found.');
+//     }
+//   } catch (error) {
+//     console.error('Error fetching minimal pairs:', error);
+//     return [];
+//   }
+// }
+
+// const minimalPairs = getAllMinimalPairs();
+
+// // Function to filter minimal pairs by sound type
+// function filterPairsBySoundType(minimalPairs, soundType) {
+//   return minimalPairs.filter(pair => pair.sound_type === soundType);
+// }
+
+// // Function to filter minimal pairs by position in word
+// function filterPairsByPosition(minimalPairs, position) {
+//   return minimalPairs.filter(pair => pair.position_in_word === position);
+// }
+
+// // Function to filter minimal pairs by a specific pair of sounds
+// function filterPairsBySoundPair(minimalPairs, soundPair) {
+//   return minimalPairs.filter(pair =>
+//     pair.sound_pair === soundPair
+//   );
+// }
+
+// // Function to get minimal pairs based on sound pair, sound type, and position in word
+// async function getPairsByCriteria(soundType, soundPair, positionInWord) {
+//     try {
+//       const snapshot = await databaseRef.once('value');
+//       const minimalPairsData = snapshot.val();
+  
+//       if (minimalPairsData) {
+//         const minimalPairs = Object.values(minimalPairsData);
+  
+//          // Filter minimal pairs based on provided criteria
+//       const filteredPairs = minimalPairs.filter(pair =>
+//         (soundPair === null || pair.sound_pair === soundPair) &&
+//         (soundType === null || pair.sound_type === soundType) &&
+//         (positionInWord === null || pair.position_in_word === positionInWord)
+//       );
+  
+//         return filteredPairs;
+//       } else {
+//         throw new Error('No minimal pairs found.');
+//       }
+//     } catch (error) {
+//       console.error('Error getting minimal pairs:', error);
+//       return [];
+//     }
+//   }
+
+  // Function to download an image from Firebase Storage
+export default async function downloadImageFromStorage(imageUrl) {
+    try {      
+        const storage = getStorage();
+        const reff = sref(storage, 'Images/flowers.jpeg');
+
+  
+      const response = await getDownloadURL(reff);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error('Error downloading image:', error);
+      return null;
+    }
+    // try {
+    //     const storage = getStorage();
+    //     const url = await getDownloadURL(sRef(storage, 'Images/redFlower.jpg'));
+    //     // `url` is the download URL for 'images/stars.jpg'
+          
+    //     // Or inserted into an <img> element
+    //     const img = document.getElementById('myimg');
+    //     img.setAttribute('src', url);
+
+    //     return img;
+    //   } catch (error) {
+    //     // Handle any errors
+    //   }
+      
+  }
+
+// // // Call the functions and log the filtered minimal pairs
+// //
+// // (async () => {
+// //   const allPairs = await getAllMinimalPairs();
+// //   const soundFilteredPairs = filterPairsBySoundType(allPairs, desiredSoundType);
+// //   const positionFilteredPairs = filterPairsByPosition(soundFilteredPairs, desiredPosition);
+// //   const soundPairFilteredPairs = filterPairsBySoundPair(positionFilteredPairs, desiredSoundPair);
+
+// //   if (soundPairFilteredPairs.length > 0) {
+// //     console.log(`Retrieved ${soundPairFilteredPairs.length} minimal pairs with sound type ${desiredSoundType}, position ${desiredPosition}, and sound pair ${desiredSoundPair}:`, soundPairFilteredPairs);
+// //   } else {
+// //     console.log(`No minimal pairs found with sound type ${desiredSoundType}, position ${desiredPosition}, and sound pair ${desiredSoundPair}.`);
+// //   }
+// // })();
