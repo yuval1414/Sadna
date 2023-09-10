79d446a76a3cada33e4f0e1cdb59d1a2471e6222
Yuval Hochman <yuvalho@mta.ac.il>
2023-09-10 11:31:17 +0300

    adding women reactions

--- minimal-pairs-app/src/component/utils/Reaction.js
+++ minimal-pairs-app/src/component/utils/Reaction.js
@@ -1,5 +1,5 @@
 //positive reactions
-export const positiveReactions = [
+export const positiveReactionsMan = [
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FPositive%2F%D7%90%D7%99%D7%9F%20%D7%9B%D7%9E%D7%95%D7%9B%D7%9D!.wav?alt=media&token=0604af36-52a3-42a3-a47e-10df58e36478",
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FPositive%2F%D7%94%D7%9E%D7%A9%D7%99%D7%9B%D7%95%20%D7%9B%D7%9A!.wav?alt=media&token=dc499570-1c5d-4f8c-bbb2-c116f05a21ce",
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FPositive%2F%D7%9B%D7%9C%20%D7%94%D7%9B%D7%91%D7%95%D7%93!.wav?alt=media&token=8ed5c4fb-02a2-407f-aafd-be10cd33e48e",
@@ -8,7 +8,7 @@ export const positiveReactions = [
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FPositive%2F%D7%AA%D7%95%D7%AA%D7%97%D7%99%D7%9D!.wav?alt=media&token=72e9fc2f-6855-4c43-ae9d-dc8609384034"
 ];
 
-export const negativeReactions = [
+export const negativeReactionsMan = [
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FNegative%2F%D7%91%D7%95%D7%90%D7%95%20%D7%A0%D7%A0%D7%A1%D7%94%20%D7%A9%D7%95%D7%91.wav?alt=media&token=fe674cfe-072e-4005-a334-211d16ddb7f0",
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FNegative%2F%D7%91%D7%97%D7%A8%D7%95%20%D7%A9%D7%95%D7%91.wav?alt=media&token=9cac48ae-da17-40de-afbb-47175bc0ebfb",
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FNegative%2F%D7%94%D7%9E%D7%A9%D7%99%D7%9B%D7%95%20%D7%9C%D7%A0%D7%A1%D7%95%D7%AA.wav?alt=media&token=027a4faf-564e-4182-a32c-7065b40d9ccc",
@@ -17,12 +17,39 @@ export const negativeReactions = [
     "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FMen%2FNegative%2F%D7%A0%D7%A1%D7%95%20%D7%A9%D7%A0%D7%99%D7%AA.wav?alt=media&token=8e784862-77e7-405d-ae57-841ee037043e"
 ];
 
-export function randomReaction(posReaction) {
-    if (posReaction) {
-        return positiveReactions[Math.round(Math.random() * 6)];
+export const positiveReactionsWoman = [
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%90%D7%99%D7%9F%20%D7%9B%D7%9E%D7%95%D7%9B%D7%9D.wav?alt=media&token=250ebcec-41e0-44b6-a400-8d28f8e1fa13",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%94%D7%9E%D7%A9%D7%99%D7%9B%D7%95%20%D7%9B%D7%9A.wav?alt=media&token=1811337b-d6e2-4b9c-b536-eaaed91207da",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%9B%D7%9C%20%D7%94%D7%9B%D7%91%D7%95%D7%93.wav?alt=media&token=35366cd0-9a55-41f6-b2ca-dc7e2f7b600b",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%9E%D7%A2%D7%95%D7%9C%D7%94.wav?alt=media&token=0ad86d6b-97b2-4604-8b8d-3b02ed7b6dc5",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%A2%D7%91%D7%95%D7%93%D7%94%20%D7%98%D7%95%D7%91%D7%94.wav?alt=media&token=c61cbe3b-d4e0-4e19-8d81-1d0955a55201",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FPositive%2F%D7%AA%D7%95%D7%AA%D7%97%D7%99%D7%9D.wav?alt=media&token=e7d93c1e-275e-4ef7-9dd7-33ad4582afc8"
+
+];
+
+export const negativeReactionsWoman = [
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%91%D7%95%D7%90%D7%95%20%D7%A0%D7%A0%D7%A1%D7%94%20%D7%A9%D7%95%D7%91.wav?alt=media&token=7511bce8-0854-4f29-a7a4-7a5b20715069",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%91%D7%97%D7%A8%D7%95%20%D7%A9%D7%95%D7%91.wav?alt=media&token=6e255882-66f6-4ba4-b5f6-fa959aed30df",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%94%D7%9E%D7%A9%D7%99%D7%9B%D7%95%20%D7%9C%D7%A0%D7%A1%D7%95%D7%AA.wav?alt=media&token=7b6c646b-af44-4377-9828-bb3603b6d3fb",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%A0%D7%99%D7%A1%D7%99%D7%95%D7%9F%20%D7%A0%D7%95%D7%A1%D7%A3.wav?alt=media&token=51b7cd69-bc57-46c1-b1c6-b975f7baa80d",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%A0%D7%A1%D7%95%20%D7%A9%D7%95%D7%91.wav?alt=media&token=baf1ab0b-20d6-4aec-a2b1-69bb47123eaa",
+    "https://firebasestorage.googleapis.com/v0/b/hear-me-out-785e2.appspot.com/o/Sounds%2FReactions%2FWoman%2FNegative%2F%D7%A0%D7%A1%D7%95%20%D7%A9%D7%A0%D7%99%D7%AA.wav?alt=media&token=2f0b8c81-19c8-4f34-9df6-6f5e362083d3"
+];
+
+export function randomReaction(posReaction, voice) {
+    console.log("voice to recived : " + voice);
+
+    if (posReaction && (voice === 'גבר')) {
+        return positiveReactionsMan[Math.round(Math.random() * 6)];
     }
-    else{
-        return negativeReactions[Math.round(Math.random() * 6)];
+    else if (voice === 'גבר') {
+        return negativeReactionsMan[Math.round(Math.random() * 6)];
+    }
+    else if (posReaction && (voice === 'אישה')) {
+        return positiveReactionsWoman[Math.round(Math.random() * 6)];
+    }
+    else if (voice === 'אישה') {
+        return negativeReactionsWoman[Math.round(Math.random() * 6)];
     }
 
 }
